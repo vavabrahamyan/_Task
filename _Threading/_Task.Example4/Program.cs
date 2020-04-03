e33b465771e5276d88d3299553114be7f9b8edc9
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-04-03 17:46:21 +0400

    c-2

---
+++ _Threading/_Task.Example4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _Task.Example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = Task.Run(() => Thread.Sleep(2000));
+            Console.WriteLine($"Task(t1)'s status {t1.Status}");
+
+            try
+            {
+                Console.WriteLine($"Task(t1)'s status {t1.Status}");
+                //
+                t1.Wait();
+                Console.WriteLine($"Task(t1)'s status {t1.Status}");
+            }
+
+            catch(AggregateException ex)
+            {
+                Console.WriteLine($"Task(t1) in Exception {ex.Message}");
+            }
+
+            //Console.ReadLine();
+        }
+    }
+}
