3d31f2703710be88ac6e824f75aa7c3345a06d84
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-04-03 16:19:06 +0400

    c-2

---
+++ _Threading/_Task.Example.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _Task.Example._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<Object> action = (Object obj) =>
+            {
+                Console.WriteLine($"Task = {Task.CurrentId}, obj = {obj}, " +
+                    $"Thread = {Thread.CurrentThread.ManagedThreadId}");
+            };
+
+            //create task but don't start it
+            Task task1 = new Task(action, "Alpha");
+
+            //construct a startid task
+            Task task2 = Task.Factory.StartNew(action, "beta");
+            //block the main thread to demonstrate that t2 is executing
+            task2.Wait();
+
+            //wait for the task to finish
+            task1.Wait();
+
+            //construct the strated task using Task.Run
+            string taskData = "delta";
+            Task task3 = Task.Run(() =>
+                {
+                    Console.WriteLine($"Task = {Task.CurrentId}, obj = {taskData}, " +
+                        $"Thread = {Thread.CurrentThread.ManagedThreadId}");
+                }
+            );
+
+            //wait for the task to finish
+            task3.Wait();
+
+            //construct an unstarted task
+            Task task4 = new Task(action, "Gamma");
+            //run it synchronously
+            task4.RunSynchronously();
+            //...
+            task4.Wait();
+        }
+    }
+}
---
+++ _Threading/_Task.Example.3(Metanit)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _Task.Example._3_Metanit_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task1 = new Task<int>(() => Factorial(5));
+            task1.Start();
+
+            Console.WriteLine($"Fatorial of number 5 is {task1.Result}");
+
+            Task<Book> task2 = new Task<Book>(() =>
+            {
+                return new Book { Title = "World and war", Author = "Lev  Tolstoy" };
+            });
+            task2.Start();
+
+            Book b = task2.Result;  // ожидаем получение результата
+            Console.WriteLine($"Book namespase: {b.Title}, autor: {b.Author}");
+
+            Console.ReadLine();
+        }
+
+
+        static int Factorial(int x)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
